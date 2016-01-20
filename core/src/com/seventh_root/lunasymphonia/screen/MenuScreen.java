7ab4f5f34f89dd32f4e8144c0b24fe1cc87c03fe
Ross Binden <alyphen@gmail.com>
2016-01-20 21:54:56 +0000

    Update logo

--- core/src/com/seventh_root/lunasymphonia/screen/MenuScreen.java
+++ core/src/com/seventh_root/lunasymphonia/screen/MenuScreen.java
@@ -1,5 +1,6 @@
 package com.seventh_root.lunasymphonia.screen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -7,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import static com.badlogic.gdx.Gdx.files;
+import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
 
 public class MenuScreen extends ScreenAdapter {
 
@@ -18,12 +20,14 @@ public class MenuScreen extends ScreenAdapter {
         camera = new OrthographicCamera();
         camera.setToOrtho(false);
         spriteBatch = new SpriteBatch();
-        logoSprite = new Sprite(new Texture(files.internal("logo.png")));
+        logoSprite = new Sprite(new Texture(files.internal("logo_darkbg.png")));
         logoSprite.setPosition(0, 600 - logoSprite.getHeight());
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0F, 0F, 0F, 1F);
+        Gdx.gl.glClear(GL_COLOR_BUFFER_BIT);
         spriteBatch.setProjectionMatrix(camera.combined);
         spriteBatch.begin();
         logoSprite.draw(spriteBatch);
